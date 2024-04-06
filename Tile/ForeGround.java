462af8bec3f3d535ec552bb19f737512a8997e64
Sting421 <aldrinjohnvitorillo0229@gmail.com>
2024-04-06 19:32:09 +0800

    change

--- Tile/ForeGround.java
+++ Tile/ForeGround.java
@@ -19,7 +19,7 @@ public class ForeGround {
 
     public ForeGround(GamePanel GP) {
         this.GP = GP;
-        tiles = new Tile[10];
+        tiles = new Tile[12];
         mapTileNumber = new int[GP.maxWorldCol][GP.maxWorldRow];
         getTileImage();
         loadMap("/res/maps/ForeGround.txt");
@@ -29,7 +29,7 @@ public class ForeGround {
         try {
 
             tiles[0] = new Tile();
-            tiles[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/transparent.png")));
+            tiles[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/1transparent.png")));
 
             tiles[1] = new Tile();
             tiles[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/CornerWallLeft.png")));
@@ -43,6 +43,37 @@ public class ForeGround {
             tiles[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/EndingWallLower.png")));
             tiles[3].collision = true;
 
+            tiles[4] = new Tile();
+            tiles[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/EndingWallUpper.png")));
+            tiles[4].collision = true;
+
+            tiles[5] = new Tile();
+            tiles[5].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/SideWall.png")));
+            tiles[5].collision = true;
+
+            tiles[6] = new Tile();
+            tiles[6].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/UpperCornerLeft.png")));
+            tiles[6].collision = true;
+
+            tiles[7] = new Tile();
+            tiles[7].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/UpperCornerRight.png")));
+            tiles[7].collision = true;
+
+            tiles[8] = new Tile();
+            tiles[8].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/UpperWallMid.png")));
+            tiles[8].collision = true;
+
+            tiles[9] = new Tile();
+            tiles[9].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/WallDownLeft.png")));
+            tiles[9].collision = true;
+
+            tiles[10] = new Tile();
+            tiles[10].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/wallDownRight.png")));
+            tiles[10].collision = true;
+
+            tiles[11] = new Tile();
+            tiles[11].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tiles/Wall/wallMid.png")));
+            tiles[11].collision = true;
 
 
         }catch (IOException e){
