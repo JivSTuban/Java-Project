27328cb5078f02740b135e797724b0ab2e3fa9e7
Sting421 <aldrinjohnvitorillo0229@gmail.com>
2024-04-07 01:59:53 +0800

    update map with water

--- GUI/GamePanel.java
+++ GUI/GamePanel.java
@@ -2,7 +2,6 @@ package GUI;
 
 import Controles.KeyHandler;
 import Entities.Player;
-import Tile.ChoiChoi;
 import Tile.ForeGround;
 import Tile.TileManager;
 
@@ -27,7 +26,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int FPS = 60;
     TileManager tileManager = new TileManager(this);
     ForeGround foreGround = new ForeGround(this);
-    ChoiChoi choiChoi = new ChoiChoi(this);
+
 
     KeyHandler keyH = new KeyHandler();
 
@@ -83,7 +82,6 @@ public class GamePanel extends JPanel implements Runnable{
         Graphics2D g2 = (Graphics2D) g;
         tileManager.draw(g2);
         foreGround.draw(g2);
-        choiChoi.draw(g2);
         player.draw(g2);
 
         g2.dispose();
