d21d76aa6148f2def85983286aa2989ffc97abb1
scott <scottbenz03@gmail.com>
2024-04-18 00:38:10 +0800

    added npc console

---
+++ Entities/NPC_Console.java
@@ -0,0 +1,35 @@
+package Entities;
+
+import GUI.GamePanel;
+
+public class NPC_Console extends Entity{
+    public NPC_Console(GamePanel gp) {
+        super(gp);
+        NPC_name = "console";
+        collision = true;
+        direction = "left";
+        getImage();
+        setDialogue();
+    }
+
+    public void getImage() {
+        up1 = setup("/res/npc_console/entryrobot1");
+        down1 = setup("/res/npc_console/entryrobot2");
+        down2 = setup("/res/npc_console/entryrobot3");
+    }
+    public void setDialogue(){
+        dialogues[0] = "Welcome to Mitsu Realm!";
+        dialogues[1] = " \n\n Realm of Technology Veemax, the technomancer, deptly manipulates \nmachines and technology.";
+
+    }
+    public void speak(){
+        gp.ui.currentDialogue = dialogues[0] + dialogues[1];
+        switch (gp.player.direction){
+            case "up":direction= "down";break;
+            case "down":direction= "up";break;
+            case "left":direction= "right";break;
+            case "right":direction= "left";break;
+
+        }
+}
+}
